3bc12f64c0c4758270a2376019a4fac454736120
aliamerj <aliamer19ali@gmail.com>
2024-08-10 00:27:31 +0300

    code update

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
-const file = @import("utils/file.zig");
-const freq = @import("utils/frequency.zig");
+const f = @import("utils/file.zig");
 const huffman = @import("huffman/encoding.zig");
 
 pub fn main() !void {
@@ -13,11 +12,13 @@ pub fn main() !void {
 
     if (args.len < 2) return std.log.err("error: file path is required, please provide file path as in example \n tcrunch -- file path\n", .{});
 
-    const file_content = file.readFile(args[1], &allocator) catch |err| {
+    const file_content = f.readFile(args[1], &allocator) catch |err| {
         std.log.err("Failed to read file: {}\n", .{err});
         return;
     };
 
-    var freq_table = try freq.calculateFrequency(file_content, allocator);
+    var content = f.FileContent.init(allocator, file_content);
+
+    var freq_table = try content.calculateFrequency();
     try huffman.encoding(&freq_table, allocator);
 }
--- src/utils/file.zig
+++ src/utils/file.zig
@@ -1,5 +1,35 @@
 const std = @import("std");
 
+const Allocator = std.mem.Allocator;
+const Hash_map = std.hash_map.AutoHashMap(u8, usize);
+
+pub const FileContent = struct {
+    allocator: Allocator,
+    data: []const u8,
+
+    pub fn init(allocator: Allocator, data: []const u8) FileContent {
+        return FileContent{
+            .allocator = allocator,
+            .data = data,
+        };
+    }
+
+    pub fn getFileData(self: *FileContent) []const u8 {
+        return self.data;
+    }
+
+    pub fn calculateFrequency(self: *FileContent) !Hash_map {
+        var freq_table = Hash_map.init(self.*.allocator);
+
+        for (self.*.data) |c| {
+            var count = freq_table.get(c) orelse 0;
+            count += 1;
+            try freq_table.put(c, count);
+        }
+        return freq_table;
+    }
+};
+
 pub fn readFile(file_path: []const u8, allocator: *std.mem.Allocator) ![]u8 {
     const file = try std.fs.cwd().openFile(file_path, .{});
     defer file.close();
@@ -12,4 +42,24 @@ pub fn readFile(file_path: []const u8, allocator: *std.mem.Allocator) ![]u8 {
     if (file_size != bytes_read) return error.ReadError;
 
     return buffer;
+}
+
+const expect = std.testing.expect;
+
+test "calculate frequency and get data" {
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+    const data = "4aaabbc 4";
+
+    var content = FileContent.init(allocator, data);
+    var freq_table = content.calculateFrequency() catch unreachable;
+    defer freq_table.deinit();
+
+    try std.testing.expectEqualStrings(content.getFileData(), data);
+    try expect(freq_table.get('a') == 3);
+    try expect(freq_table.get('b') == 2);
+    try expect(freq_table.get('c') == 1);
+    try expect(freq_table.get(' ') == 1);
+    try expect(freq_table.get('4') == 2);
 }
